dbecf6e2670bf4887e140f98e37d774834165a86
nam <nphan@atomiton.com>
2020-05-22 21:31:41 -0700

    update left bar

--- project-4495/projects/simulator-app/src/app/app-routing.module.ts
+++ project-4495/projects/simulator-app/src/app/app-routing.module.ts
@@ -1,13 +1,21 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {RestaurantComponent} from './routes/restaurant/restaurant.component';
+import {MapComponent} from "@app/routes/map/map.component";
+import {CustomerComponent} from "@app/routes/customer/customer.component";
+import {OrderComponent} from "@app/routes/order/order.component";
+import {CourierComponent} from "@app/routes/courier/courier.component";
 
 const routes: Routes = [
   {
     path: 'admin',
     children: [
-      {path: '', redirectTo: 'restaurant', pathMatch: 'full'},
+      {path: '', redirectTo: 'map', pathMatch: 'full'},
       {path: 'restaurant', component: RestaurantComponent},
+      {path: 'map', component: MapComponent},
+      {path: 'customer', component: CustomerComponent},
+      {path: 'order', component: OrderComponent},
+      {path: 'courier', component: CourierComponent},
     ]
   },
   {path: '', redirectTo: 'admin', pathMatch: 'full'},
--- project-4495/projects/simulator-app/src/app/routes/routes.module.ts
+++ project-4495/projects/simulator-app/src/app/routes/routes.module.ts
@@ -3,9 +3,13 @@ import {CommonModule} from '@angular/common';
 import {RestaurantComponent} from './restaurant/restaurant.component';
 import {AngularFireModule} from '@angular/fire';
 import {environment} from '../../environments/environment';
+import { MapComponent } from './map/map.component';
+import { CustomerComponent } from './customer/customer.component';
+import { CourierComponent } from './courier/courier.component';
+import { OrderComponent } from './order/order.component';
 
 @NgModule({
-  declarations: [RestaurantComponent],
+  declarations: [RestaurantComponent, MapComponent, CustomerComponent, CourierComponent, OrderComponent],
   imports: [
     CommonModule,
     AngularFireModule.initializeApp(environment.firebase),
