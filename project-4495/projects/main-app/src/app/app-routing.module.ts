4700b3988ccfe05e10fbac827db1a2e005282722
Karan-95 <karan.singh.990@live.com>
2020-06-05 12:58:43 -0700

    Login Angular

--- project-4495/projects/main-app/src/app/app-routing.module.ts
+++ project-4495/projects/main-app/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {RestaurantsComponent} from './mainroute/restaurants/restaurants.component';
+import {LoginComponent} from './mainroute/login/login.component';
 
 const routes: Routes = [
   {path: 'rest', component: RestaurantsComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
--- project-4495/projects/main-app/src/app/mainroute/mainroute.module.ts
+++ project-4495/projects/main-app/src/app/mainroute/mainroute.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RestaurantsComponent } from "./restaurants/restaurants.component";
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
-  declarations: [RestaurantsComponent],
+  declarations: [RestaurantsComponent, LoginComponent],
   imports: [
     CommonModule
   ]
