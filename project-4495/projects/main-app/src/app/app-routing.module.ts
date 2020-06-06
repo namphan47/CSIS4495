f4fae9da6a5f953d59a7e13f799079a772c21702
Karan-95 <karan.singh.990@live.com>
2020-06-05 20:25:37 -0700

    Sign Up Angular

--- project-4495/projects/main-app/src/app/app-routing.module.ts
+++ project-4495/projects/main-app/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {RestaurantsComponent} from './mainroute/restaurants/restaurants.component';
 import {LoginComponent} from './mainroute/login/login.component';
+import {SignupComponent} from "./mainroute/signup/signup.component";
 
 const routes: Routes = [
   {path: 'rest', component: RestaurantsComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent}
 ];
 
 @NgModule({
--- project-4495/projects/main-app/src/app/mainroute/mainroute.module.ts
+++ project-4495/projects/main-app/src/app/mainroute/mainroute.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RestaurantsComponent } from "./restaurants/restaurants.component";
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 
 @NgModule({
-  declarations: [RestaurantsComponent, LoginComponent],
+  declarations: [RestaurantsComponent, LoginComponent, SignupComponent],
   imports: [
     CommonModule
   ]
