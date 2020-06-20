b3e16fd73663340ff83689c4a4c01e44fb27a045
TushitNag11 <kanurit@student.douglascollege.ca>
2020-06-20 16:47:23 -0700

    made changes to restaurant details

--- project-4495/projects/main-app/src/app/mainroute/restaurant-details/restaurant-details.component.ts
+++ project-4495/projects/main-app/src/app/mainroute/restaurant-details/restaurant-details.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {FirebaseDataService, Meal, Restaurant} from 'library-app';
+
 
 @Component({
   selector: 'app-restaurant-details',
@@ -9,20 +11,41 @@ import {ActivatedRoute} from '@angular/router';
 export class RestaurantDetailsComponent implements OnInit, OnDestroy {
   private sub: any;
   id: string;
+  private restaurants: Restaurant[];
+  private meals: Meal[];
+  private restaurantDetail: Restaurant;
+  private detail;
 
-  constructor(private route: ActivatedRoute) {
+
+  constructor(private route: ActivatedRoute, private _FirebaseDataService: FirebaseDataService) {
+
     this.sub = this.route.params.subscribe(params => {
       this.id = params['id'];
+      const promise = this._FirebaseDataService.getRestaurant();
+      promise
+        .then((rs) => {
+          this.restaurants = rs;
+          console.log(this.detail);
+          console.log(this.restaurants);
+
+          for (let i = 0 ; i < this.restaurants.length; i++) {
+            if (this.restaurants[i].id = this.id) {
+              this.detail = this.restaurants[i];
+            }
+          }
+          console.log(this.detail);
+        });
+
+
     });
+
+
+  }
+
+  ngOnDestroy(): void {
   }
 
   ngOnInit(): void {
-
-
   }
-
-  ngOnDestroy() {
-    this.sub.unsubscribe();
-  }
-
 }
+
