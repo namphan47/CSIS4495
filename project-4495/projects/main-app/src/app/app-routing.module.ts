4acf7e439dccece21efa14a032df86ebda5a96e3
Karan-95 <karan.singh.990@live.com>
2020-06-25 01:03:31 -0700

    Restaurant JSON updated

--- project-4495/projects/main-app/src/app/app-routing.module.ts
+++ project-4495/projects/main-app/src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   {
     path: 'main',
     children: [
-      {path: '', redirectTo: 'login', pathMatch: 'full'},
+      {path: '', redirectTo: 'signup', pathMatch: 'full'},
       {path: 'rest', component: RestaurantsComponent},
       {path: 'rest-details/:id', component: RestaurantDetailsComponent},
       {path: 'login', component: LoginComponent},
