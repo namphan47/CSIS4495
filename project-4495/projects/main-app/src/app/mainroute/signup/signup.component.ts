4feec8fa803e29fb109448ad7d9c03aaf05de8ae
nam <naph47@gmail.com>
2020-08-03 16:06:10 -0700

    made design changes to sign up page

--- project-4495/projects/main-app/src/app/mainroute/signup/signup.component.ts
+++ project-4495/projects/main-app/src/app/mainroute/signup/signup.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {Router} from "@angular/router";
-import {Customer, FirebaseDataService} from "library-app";
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {Router} from '@angular/router';
+import {Customer, FirebaseDataService} from 'library-app';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 import validate = WebAssembly.validate;
 
 
@@ -16,20 +16,20 @@ export class SignupComponent implements OnInit {
   signupFormGroup: FormGroup;
 
 
-
-  constructor(private router: Router,  private _FirebaseDataService: FirebaseDataService) {
+  constructor(private router: Router, private _FirebaseDataService: FirebaseDataService) {
   }
 
   ngOnInit(): void {
     this.signupFormGroup = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required, Validators.pattern(new RegExp('^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$'))]),
       repassword: new FormControl('', [Validators.required]),
-      fullname: new FormControl('', [Validators.required]),
-      phone: new FormControl('', [Validators.required]),
+      fullname: new FormControl('', [Validators.required, Validators.pattern(new RegExp('^[a-zA-Z]+(([\',. -][a-zA-Z ])?[a-zA-Z]*)*$'))]),
+      phone: new FormControl('', [Validators.required, Validators.pattern(new RegExp('^\\D?(\\d{3})\\D?\\D?(\\d{3})\\D?(\\d{4})$'))]),
       address: new FormControl('', [Validators.required])
     });
   }
+
   // onInputChange($event: any) {
   //   console.log(this.inputData);
   // }
@@ -41,8 +41,6 @@ export class SignupComponent implements OnInit {
     let isValid = true;
 
     for (let key in this.signupFormGroup.controls) {
-      console.log(key);
-      console.log(this.signupFormGroup.get(key));
       if (this.signupFormGroup.get(key).invalid) {
         isValid = false;
       }
@@ -50,28 +48,24 @@ export class SignupComponent implements OnInit {
 
     if (!isValid) {
       alert('The form is invalid');
-    }
-    else
-    {
+    } else {
       this.customer = new Customer({});
-      this.customer.email = this.signupFormGroup.get("email").value;
-      this.customer.address = this.signupFormGroup.get("address").value;
-      this.customer.name = this.signupFormGroup.get("fullname").value;
-      this.customer.password = this.signupFormGroup.get("password").value;
-      this.customer.phone_no = this.signupFormGroup.get("phone").value;
+      this.customer.email = this.signupFormGroup.get('email').value;
+      this.customer.address = this.signupFormGroup.get('address').value;
+      this.customer.name = this.signupFormGroup.get('fullname').value;
+      this.customer.password = this.signupFormGroup.get('password').value;
+      this.customer.phone_no = this.signupFormGroup.get('phone').value;
       console.log(this.customer);
       this._FirebaseDataService.signUp(this.customer)
         .then((cus) => {
-          console.log("cus = " + cus);
-          if(cus == true) {
-            this.router.navigate(['/main/', 'login'])
+          console.log('cus = ' + cus);
+          if (cus === true) {
+            this.router.navigate(['/main/', 'login']);
+          } else {
+            window.alert('Error occurred');
           }
-          else
-          {
-            window.alert("Error occurred");
-          }
-          ;});
-      //this.router.navigate(['/main/', 'login']);
+        });
+      // this.router.navigate(['/main/', 'login']);
     }
   }
 }
