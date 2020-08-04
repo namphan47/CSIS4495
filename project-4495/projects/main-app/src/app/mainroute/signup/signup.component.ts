e97861f1a2769e50a042496f3b854a25f92f8cb0
nam <naph47@gmail.com>
2020-08-03 17:07:47 -0700

    modification done to login page ui

--- project-4495/projects/main-app/src/app/mainroute/signup/signup.component.ts
+++ project-4495/projects/main-app/src/app/mainroute/signup/signup.component.ts
@@ -2,7 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {Customer, FirebaseDataService} from 'library-app';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
-import validate = WebAssembly.validate;
 
 
 @Component({
