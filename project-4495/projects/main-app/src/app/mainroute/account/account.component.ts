87f6c655d9207a7c510fe495166149a032f27e51
nam <naph47@gmail.com>
2020-08-03 17:45:34 -0700

    account page functionality is done and little changes

--- project-4495/projects/main-app/src/app/mainroute/account/account.component.ts
+++ project-4495/projects/main-app/src/app/mainroute/account/account.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Customer, FirebaseDataService} from 'library-app';
 
 @Component({
   selector: 'app-account',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountComponent implements OnInit {
 
-  constructor() { }
+  customers: Customer[];
+  customerID: String;
+  customer;
+
+  constructor(private _FirebaseDataService: FirebaseDataService) {
+  }
 
   ngOnInit(): void {
+    // Retrieve customers by calling  API using promise
+
+    const customerPromise = this._FirebaseDataService.getCustomer();
+
+    customerPromise.then((cx) => {
+
+      this.customers = cx;
+
+      //getting authorised customer id from local storage
+
+      this.customerID = localStorage.getItem('CustomerID');
+      // getting details of the current customer by comparing customer id with customers array
+      for (let i = 0; i < this.customers.length; i++) {
+
+        if (this.customerID === this.customers[i].id) {
+          this.customer = this.customers[i];
+        }
+
+      }
+
+
+    });
+
   }
 
 }
