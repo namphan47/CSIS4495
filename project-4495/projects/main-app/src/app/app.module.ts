555ebf65085063fd944bbd94f3e6fc0ef8962cda
nam <nphan@atomiton.com>
2020-08-04 20:51:52 -0700

    build

--- project-4495/projects/main-app/src/app/app.module.ts
+++ project-4495/projects/main-app/src/app/app.module.ts
@@ -5,11 +5,12 @@ import {AppComponent} from './app.component';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {HttpClientModule} from '@angular/common/http';
 import {MainrouteModule} from './mainroute/mainroute.module';
-import {AppRoutingModule} from "./app-routing.module";
-import {RouterModule} from "@angular/router";
-import {NguiMapModule} from "@ngui/map";
-import {AngularFireModule} from "@angular/fire";
-import {environment} from "../../../admin-app/src/environments/environment";
+import {AppRoutingModule} from './app-routing.module';
+import {RouterModule} from '@angular/router';
+import {NguiMapModule} from '@ngui/map';
+import {AngularFireModule} from '@angular/fire';
+import {environment} from '../../../admin-app/src/environments/environment';
+import {HashLocationStrategy, LocationStrategy} from '@angular/common';
 
 
 @NgModule({
@@ -26,7 +27,9 @@ import {environment} from "../../../admin-app/src/environments/environment";
     AngularFireModule.initializeApp(environment.firebase),
     NguiMapModule.forRoot({apiUrl: 'https://maps.google.com/maps/api/js?key=AIzaSyDrnDCTwDNyiqxi-qkY1wMRCpbBMA8LFYc'}),
   ],
-  providers: [],
+  providers: [
+    {provide: LocationStrategy, useClass: HashLocationStrategy},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
